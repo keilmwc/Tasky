dddc549db0958b16acbf6130b5f93de5f24f4bd1
Keil Carpenter <keilcarpenter@protonmail.com>
2017-11-22 11:03:08 +1300

    GUI tweaks

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const { app, BrowserWindow } = electron;
 let mainWindow;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        width: 300,
+        height: 500,
+        frame: false,
+        resizable: false
+    });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 });
