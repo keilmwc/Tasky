3a9712329f9f53c21f6abc33a051658b5fde9421
Keil Carpenter <keilcarpenter@protonmail.com>
2017-11-23 08:28:21 +1300

    Toggle window visibility via tray icon

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const { app, BrowserWindow, Tray } = electron;
 const path =require('path');
 
 let mainWindow;
+let trayIcon;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         width: 300,
         height: 500,
         frame: false,
-        resizable: false
+        resizable: false,
+        show: false
     });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
@@ -17,5 +19,14 @@ app.on('ready', () => {
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
     // New tray icon
-    new Tray(iconPath);
+    trayIcon = new Tray(iconPath);
+
+    trayIcon.on('click', () => {
+        if(mainWindow.isVisible()){
+            mainWindow.hide();
+        }else{
+            mainWindow.show();
+        }
+
+    });
 });
