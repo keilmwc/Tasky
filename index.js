e4367c6b53aba29eea78170122a8c316d451b729
Keil Carpenter <keilcarpenter@protonmail.com>
2017-11-23 10:53:32 +1300

    Refactor

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const electron = require('electron');
-const { app, BrowserWindow, Tray } = electron;
+const { app, BrowserWindow} = electron;
 const path =require('path');
+const TimerTray = require('./app/timer-tray.js');
 
 let mainWindow;
-let trayIcon;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
@@ -19,27 +19,5 @@ app.on('ready', () => {
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
     // New tray icon
-    trayIcon = new Tray(iconPath);
-
-    trayIcon.on('click', (event, bounds) => {
-        // Click event bounds
-        const { x, y } = bounds;
-
-        // Window height and width
-        const { height, width } = mainWindow.getBounds();
-
-        if(mainWindow.isVisible()){
-            mainWindow.hide();
-        }else {
-            const yPosition = process.platform === 'darwin' ? y : y - height;
-            mainWindow.setBounds({
-                x: x - width / 2,
-                y: yPosition,
-                width,
-                height
-            });
-            mainWindow.show();
-        }
-
-    });
+    new TimerTray(iconPath, mainWindow);
 });
